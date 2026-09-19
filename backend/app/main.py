6b846f9682045e78cdbb661fb96d5d0a6371fba6
S.Jaswant <rainaathi622@gmail.com>
2026-09-19 21:01:40 +0530

    Day 3: PostgreSQL connected successfully via Supabase pooler

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
     return {"status": "Backend running fine"}
+
+@app.get("/test-db")
+def test_db_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"database": "Connected successfully"}
+    except Exception as e:
+        return {"database": "Connection failed", "error": str(e)}
